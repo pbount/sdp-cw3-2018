f7c0e68bc040586c8e75fb135be3107095454f3a
pbount <pbount01@dcs.bbk.ac.uk>
2018-04-22 22:02:13 +0300

    Setting up MessageCenterImpl, implementation for message center and MessageCenter test.

---
+++ src/MessageCenterImpl.kt
@@ -0,0 +1,34 @@
+class MessageCentreImpl(override val stringBuilder: StringBuilder) : MessageCentre {
+    override fun welcome(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun success(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun fail(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun instructions(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun guessesLeft(n: Int): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun anotherGo(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun results(inp: String, result: Pair<Int, Int>): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun secretCode(secretCode: String): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ tests/MessageCentreImplTest.kt
@@ -0,0 +1,15 @@
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MessageCentreImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun welcome() {
+    }
+}
