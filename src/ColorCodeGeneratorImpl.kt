264f97058dedfe26416ecfa964646ba726551ea0
pbount <pbount01@dcs.bbk.ac.uk>
2018-04-22 22:49:48 +0300

    Working on colorCode Tests

--- src/ColorCodeGeneratorImpl.kt
+++ src/ColorCodeGeneratorImpl.kt
@@ -1,5 +1,18 @@
-class ColorCodeGeneratorImpl : ColorCodeGenerator {
-    override fun colorCode(n: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+import java.util.*
+
+/**
+ * Responsible for generating the secret code to be cracked by
+ * the user.
+ *
+ * arrayOfPegs represents all valid pegs which can be used to generate
+ * the secret code.
+ */
+class ColorCodeGeneratorImpl(private val arrayOfPegs: Array<Peg>): ColorCodeGenerator {
+
+    /**
+     * Receives a number n and generates a code with size n.
+     */
+    override fun colorCode (n: Int) : String {
+        TODO()
     }
 }
--- tests/ColorCodeGeneratorImplTest.kt
+++ tests/ColorCodeGeneratorImplTest.kt
@@ -1,10 +1,34 @@
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class ColorCodeGeneratorImplTest {
 
+    private lateinit var obj1: ColorCodeGeneratorImpl
+    private lateinit var arrayOfPegs: Array<Peg>
+    private lateinit var listOfAcceptableValues : List<String>
+
+    @Before
+    fun beforeTest(){
+        arrayOfPegs = PegFactory.getPegs(PegColor.values())
+        listOfAcceptableValues = arrayOfPegs.map { it.color() }
+
+        obj1 = ColorCodeGeneratorImpl(arrayOfPegs)
+
+    }
+
+    /**
+     * Tests that the generated values are all acceptable.
+     */
     @Test
-    fun colorCode() {
+    fun colorCodeTest() {
+
+        for(i in 0..100000){
+            val res = obj1.colorCode(4)
+            assertTrue(res.length == 4)
+            res.forEach { assertTrue(listOfAcceptableValues.contains(it.toString())) }
+        }
+
     }
 }
