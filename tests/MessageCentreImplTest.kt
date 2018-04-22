398b0afc12762ab97c1d0d7470e8951a7e93a474
pbount <pbount01@dcs.bbk.ac.uk>
2018-04-22 22:12:13 +0300

    Added Success and Failure tests

--- tests/MessageCentreImplTest.kt
+++ tests/MessageCentreImplTest.kt
@@ -21,6 +21,42 @@ class MessageCentreImplTest {
         assertTrue(res.isNotEmpty())
         assertTrue(res.startsWith("""Welcome to Mastermind""".trimIndent()))
         assertTrue(res.endsWith("Generating secret code ...."))
+    }
 
+    @Test
+    fun welcomeTestFailure(){
+        res = messageCentre.welcome()
+        assertFalse(res.isEmpty())
+        assertFalse(randomText == res)
+    }
+
+
+    @Test
+    fun successTestSuccess(){
+        res = messageCentre.success()
+        assertTrue(res.isNotEmpty())
+        assertTrue(res == ("\nYou solved the puzzle! Good job.\n"))
+    }
+
+    @Test
+    fun successTestFailure() {
+        res = messageCentre.success()
+        assertFalse(res.isEmpty())
+        assertFalse(randomText == res)
+    }
+
+    @Test
+    fun failTestSuccess() {
+        res = messageCentre.fail()
+        assertTrue(res.isNotEmpty())
+        assertTrue(res == "You did not solve the puzzle. Too bad.")
+    }
+
+    @Test
+    fun failTestFailure() {
+        res = messageCentre.fail()
+        res = messageCentre.success()
+        assertFalse(res.isEmpty())
+        assertFalse(randomText == res)
     }
 }
